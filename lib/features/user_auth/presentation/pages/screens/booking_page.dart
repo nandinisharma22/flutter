d5da839e02243cebed2d837f654b61242930951b
nandini11509 <nandini11509@gmail.com>
2024-03-31 18:24:09 +0530

    bokking page slots count

--- lib/features/user_auth/presentation/pages/screens/booking_page.dart
+++ lib/features/user_auth/presentation/pages/screens/booking_page.dart
@@ -1,4 +1,14 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:intl/intl.dart';
+import 'package:haxplore/features/user_auth/presentation/pages/models/booking_datetime_converted.dart';
+import 'package:haxplore/features/user_auth/firebase_auth_implementation/firebase_auth_services.dart';
+import 'package:haxplore/features/user_auth/presentation/pages/components/button.dart';
+import 'package:haxplore/features/user_auth/presentation/pages/utils/config.dart';
+import 'package:haxplore/features/user_auth/presentation/pages/components/custom_appbar.dart';
+import 'package:haxplore/main.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:haxplore/features/user_auth/firebase_auth_implementation/firebase_auth_services.dart';
 import 'package:haxplore/features/user_auth/presentation/pages/components/button.dart';
 import 'package:haxplore/features/user_auth/presentation/pages/utils/config.dart';
@@ -12,7 +22,6 @@ import 'package:shared_preferences/shared_preferences.dart';
 import 'package:table_calendar/table_calendar.dart';
 import 'package:intl/intl.dart';
 
-
 class BookingPage extends StatefulWidget {
   final User user;
 
@@ -20,235 +29,7 @@ class BookingPage extends StatefulWidget {
 
   @override
   State<BookingPage> createState() => _BookingPageState();
-
-
 }
-
-// class _BookingPageState extends State<BookingPage> {
-//   //declaration
-//   CalendarFormat _format = CalendarFormat.month;
-//   DateTime _focusDay = DateTime.now();
-//   DateTime _currentDay = DateTime.now();
-//   int? _currentIndex;
-//   // bool _isWeekend = false;
-//   bool _dateSelected = false;
-//   bool _timeSelected = false;
-//   final AppointmentManager _appointmentManager = AppointmentManager();
-//
-//   late String userId;
-//
-//   // String? token; //get token for insert booking date and time into database
-//
-//   // Future<void> getToken() async {
-//   //   final SharedPreferences prefs = await SharedPreferences.getInstance();
-//   //   token = prefs.getString('token') ?? '';
-//   // }
-//
-//   @override
-//   void initState() {
-//     // getToken();
-//     super.initState();
-//     // getUserID();
-//   }
-//
-//   // Future<void> getUserID() async {
-//   //   final user = FirebaseAuth.instance.currentUser;
-//   //
-//   //   if (user != null) {
-//   //     print("&this message");
-//   //     setState(() {
-//   //       userId = user.uid;
-//   //     });
-//   //   }
-//   //   else{
-//   //
-//   //   }
-//   // }
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     Config().init(context);
-//     // final doctor = ModalRoute.of(context)!.settings.arguments as Map;
-//     return Scaffold(
-//       appBar: CustomAppBar(
-//         appTitle: 'Appointment',
-//         icon: const FaIcon(Icons.arrow_back_ios),
-//       ),
-//       body: CustomScrollView(
-//         slivers: <Widget>[
-//           SliverToBoxAdapter(
-//             child: Column(
-//               children: <Widget>[
-//                 _tableCalendar(),
-//                 const Padding(
-//                   padding: EdgeInsets.symmetric(horizontal: 10, vertical: 25),
-//                   child: Center(
-//                     child: Text(
-//                       'Select your Slot',
-//                       style: TextStyle(
-//                         fontWeight: FontWeight.bold,
-//                         fontSize: 20,
-//                       ),
-//                     ),
-//                   ),
-//                 )
-//               ],
-//             ),
-//           ),
-//           // _isWeekend
-//           //     ? SliverToBoxAdapter(
-//           //   child: Container(
-//           //     padding: const EdgeInsets.symmetric(
-//           //         horizontal: 10, vertical: 30),
-//           //     alignment: Alignment.center,
-//           //     child: const Text(
-//           //       'Weekend is not available, please select another date',
-//           //       style: TextStyle(
-//           //         fontSize: 18,
-//           //         fontWeight: FontWeight.bold,
-//           //         color: Colors.grey,
-//           //       ),
-//           //     ),
-//           //   ),
-//           // )
-//           SliverGrid(
-//             delegate: SliverChildBuilderDelegate(
-//                   (context, index) {
-//                 return InkWell(
-//                   splashColor: Colors.transparent,
-//                   onTap: () {
-//                     setState(() {
-//                       _currentIndex = index;
-//                       _timeSelected = true;
-//                     });
-//                   },
-//                   child: Container(
-//                     margin: const EdgeInsets.all(5),
-//                     decoration: BoxDecoration(
-//                       border: Border.all(
-//                         color: _currentIndex == index
-//                             ? Colors.white
-//                             : Colors.black,
-//                       ),
-//                       borderRadius: BorderRadius.circular(15),
-//                       color: _currentIndex == index
-//                           ? Config.primaryColor
-//                           : null,
-//                     ),
-//                     alignment: Alignment.center,
-//                     child: Text(
-//                       '${index + 9}:00 ${index + 9 > 11 ? "PM" : "AM"}',
-//                       style: TextStyle(
-//                         fontWeight: FontWeight.bold,
-//                         color:
-//                         _currentIndex == index ? Colors.white : null,
-//                       ),
-//                     ),
-//                   ),
-//                 );
-//               },
-//               childCount: 8,
-//             ),
-//             gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-//                 crossAxisCount: 4, childAspectRatio: 1.5),
-//           ),
-//           SliverToBoxAdapter(
-//             child: Container(
-//               padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 80),
-//               child: Button(
-//                 width: double.infinity,
-//                 title: 'Book Ticket',
-//                 onPressed: () async {
-//                   final getDate = DateConverted.getDate(_currentDay);
-//                   final getDay = DateConverted.getDay(_currentDay.weekday);
-//                   final getTime = DateConverted.getTime(_currentIndex!);
-//
-//                   // Split the time string to extract the hour and minute separately
-//                   final timeParts = getTime.split(":");
-//                   final hourString = timeParts[0];
-//                   final minuteString = timeParts[1].split(" ")[0]; // Remove the AM/PM indicator
-//
-//                   // Remove non-numeric characters and concatenate the hour and minute parts
-//                   final formattedTime = hourString.replaceAll(RegExp(r'[^0-9]'), '') + minuteString;
-//
-//                   // Parse the formatted time into an integer
-//                   final getTimeInt = int.tryParse(formattedTime) ?? 0;
-//
-//
-//                   final user = FirebaseAuth.instance.currentUser;
-//
-//                   if (user != null) {
-//                     final userId = user.uid;
-//                     final getDayFormatted = DateFormat('EEE, d/M/y').format(_currentDay);
-//
-//                     await _appointmentManager.bookAppointment(getDayFormatted, getTimeInt, userId);
-//                     // MyApp.navigatorKey.currentState!.pushNamed('/success_booked');
-//                     Navigator.of(context).pushNamed('/success_booked');
-//
-//                     print(getTime);
-//                     print("\n");
-//                     print(getDayFormatted);
-//
-//
-//
-//
-//                   } else {
-//                     // Handle the case where the user is not logged in
-//                   }
-//                 },
-//
-//
-//
-//                 disable: _timeSelected && _dateSelected ? false : true,
-//
-//               ),
-//             ),
-//           ),
-//         ],
-//       ),
-//     );
-//   }
-//   //table calendar
-//   //table calendar
-//   Widget _tableCalendar() {
-//     return TableCalendar(
-//       focusedDay: _focusDay,
-//       firstDay: DateTime.now(),
-//       lastDay: DateTime.now().add(Duration(days: 60)),
-//       calendarFormat: _format,
-//       currentDay: _currentDay,
-//       rowHeight: 48,
-//       calendarStyle: const CalendarStyle(
-//         todayDecoration:
-//         BoxDecoration(color: Config.primaryColor, shape: BoxShape.circle),
-//       ),
-//       availableCalendarFormats: const {
-//         CalendarFormat.month: 'Month',
-//       },
-//       onFormatChanged: (format) {
-//         setState(() {
-//           _format = format;
-//         });
-//       },
-//       onDaySelected: ((selectedDay, focusedDay) {
-//         setState(() {
-//           _currentDay = selectedDay;
-//           _focusDay = focusedDay;
-//           _dateSelected = true;
-//
-//           //check if weekend is selected
-//           // if (selectedDay.weekday == 6 || selectedDay.weekday == 7) {
-//           //   _isWeekend = true;
-//           //   _timeSelected = false;
-//           //   _currentIndex = null;
-//           // } else {
-//           //   _isWeekend = false;
-//           // }
-//         });
-//       }),
-//     );
-//   }
-// }
 
 class _BookingPageState extends State<BookingPage> {
   // Declaration
@@ -259,6 +40,27 @@ class _BookingPageState extends State<BookingPage> {
   bool _dateSelected = false;
   bool _timeSelected = false;
   final AppointmentManager _appointmentManager = AppointmentManager();
+
+  Future<int> _getSlotCount() async {
+    String dateString = DateFormat('d.M.y').format(_currentDay);
+    DocumentSnapshot snapshot = await FirebaseFirestore.instance.collection('bookings').doc(dateString).get();
+    Map<String, dynamic>? slotData = snapshot.data() as Map<String, dynamic>?;
+
+    if (slotData != null) {
+      final getTime = DateConverted.getTime(_currentIndex!);
+      final timeParts = getTime.split(":");
+      final hourString = timeParts[0];
+      final minuteString = timeParts[1].split(" ")[0];
+      final formattedTime = hourString.replaceAll(RegExp(r'[^0-9]'), '') + minuteString;
+      final getTimeInt = int.tryParse(formattedTime) ?? 0;
+
+      Map<String, dynamic>? selectedSlotData = slotData['slot$getTimeInt'];
+      int count = selectedSlotData?['numberOfTickets'] ?? 0;
+      return count;
+    } else {
+      return 0; // Return default count or handle error
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -292,39 +94,61 @@ class _BookingPageState extends State<BookingPage> {
           SliverGrid(
             delegate: SliverChildBuilderDelegate(
                   (context, index) {
-                return InkWell(
-                  splashColor: Colors.transparent,
-                  onTap: () {
-                    setState(() {
-                      _currentIndex = index;
-                      _timeSelected = true;
-                    });
+                return FutureBuilder<DocumentSnapshot>(
+                  future: FirebaseFirestore.instance
+                      .collection('bookings')
+                      .doc(DateFormat('d.M.y').format(_currentDay))
+                      .get(),
+                  builder: (BuildContext context, AsyncSnapshot<DocumentSnapshot> snapshot) {
+                    if (snapshot.connectionState == ConnectionState.done) {
+                      if (snapshot.hasError) {
+                        return Text('Error: ${snapshot.error}');
+                      }
+
+                      final data = snapshot.data!.data() as Map<String, dynamic>?;
+                      final slotKey = 'slot${(index + 9) * 100}';
+                      final noOfTicketsBooked = data != null && data.containsKey(slotKey) ? data[slotKey]['numberOfTicketsBooked'] : 0;
+                      final availableTickets = 3 - noOfTicketsBooked;
+
+                      return InkWell(
+                        splashColor: Colors.transparent,
+                        onTap: () {
+                          setState(() {
+                            _currentIndex = index;
+                            _timeSelected = true;
+                          });
+                        },
+                        child: Container(
+                          margin: const EdgeInsets.all(5),
+                          decoration: BoxDecoration(
+                            border: Border.all(
+                              color: _currentIndex == index ? Colors.white : Colors.black,
+                            ),
+                            borderRadius: BorderRadius.circular(15),
+                            color: _currentIndex == index ? Config.primaryColor : null,
+                          ),
+                          alignment: Alignment.center,
+                          child: Text(
+                            '${index + 9}:00 ${index + 9 > 11 ? "PM" : "AM"}\nAvailable Tickets: $availableTickets',
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              color: _currentIndex == index ? Colors.white : null,
+                              fontSize: 16,
+                            ),
+                          ),
+                        ),
+                      );
+                    } else {
+                      return CircularProgressIndicator();
+                    }
                   },
-                  child: Container(
-                    margin: const EdgeInsets.all(5),
-                    decoration: BoxDecoration(
-                      border: Border.all(
-                        color: _currentIndex == index ? Colors.white : Colors.black,
-                      ),
-                      borderRadius: BorderRadius.circular(15),
-                      color: _currentIndex == index ? Config.primaryColor : null,
-                    ),
-                    alignment: Alignment.center,
-                    child: Text(
-                      '${index + 9}:00 ${index + 9 > 11 ? "PM" : "AM"}',
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        color: _currentIndex == index ? Colors.white : null,
-                      ),
-                    ),
-                  ),
                 );
               },
               childCount: 8,
             ),
             gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
               crossAxisCount: 4,
-              childAspectRatio: 1.5,
+              childAspectRatio: 1.0,
             ),
           ),
           SliverToBoxAdapter(
