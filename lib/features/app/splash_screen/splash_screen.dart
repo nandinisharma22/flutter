1f53c295483eaf04892c92f4f17601ff6e98d582
nandini11509 <nandini11509@gmail.com>
2024-03-31 15:28:26 +0530

    splash screen

--- lib/features/app/splash_screen/splash_screen.dart
+++ lib/features/app/splash_screen/splash_screen.dart
@@ -2,7 +2,8 @@ import 'package:flutter/material.dart';
 
 class SplashScreen extends StatefulWidget {
   final Widget? child;
-  const SplashScreen({super.key, this.child});
+
+  const SplashScreen({Key? key, this.child}) : super(key: key);
 
   @override
   State<SplashScreen> createState() => _SplashScreenState();
@@ -11,18 +12,39 @@ class SplashScreen extends StatefulWidget {
 class _SplashScreenState extends State<SplashScreen> {
   @override
   void initState() {
-    Future.delayed(
-        Duration(seconds: 3),(){
-      Navigator.pushAndRemoveUntil(context, MaterialPageRoute(builder: (context) => widget.child!), (route) => false);
-    }
-    );
     super.initState();
+    Future.delayed(
+      Duration(seconds: 3),
+          () {
+        Navigator.pushAndRemoveUntil(
+          context,
+          MaterialPageRoute(builder: (context) => widget.child!),
+              (route) => false,
+        );
+      },
+    );
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text("Welcome to our App"),
+      body: Container(
+        width: double.infinity, // Make the container width match the screen width
+        height: double.infinity, // Make the container height match the screen height
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/images/splashtemple.jpg'), // Replace this with your image path
+            fit: BoxFit.cover, // Cover the entire container
+          ),
+        ),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              // Add other widgets here if needed
+            ],
+          ),
+        ),
       ),
     );
   }
