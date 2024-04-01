2852c7e4249227d86ebb2dc5ce0c86d1bafbe4d7
dwsds <146217248+dwsds@users.noreply.github.com>
2024-04-01 11:47:52 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,14 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:haxplore/features/app/splash_screen/splash_screen.dart';
 import 'package:haxplore/features/user_auth/presentation/admin/admin_page.dart';
 import 'package:haxplore/features/user_auth/presentation/pages/screens/booking_page.dart';
 import 'package:haxplore/features/user_auth/presentation/pages/screens/home_page.dart';
 import 'package:haxplore/features/user_auth/presentation/pages/screens/login_page.dart';
 import 'package:haxplore/features/user_auth/presentation/pages/screens/delete.dart';
+import 'package:haxplore/features/user_auth/presentation/pages/screens/qr_gen.dart';
 import 'package:haxplore/features/user_auth/presentation/pages/screens/signup_page.dart';
 import 'package:haxplore/features/user_auth/presentation/pages/screens/success_booked.dart';
-import 'package:haxplore/ticket_booking.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-
 
 Future main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -24,26 +22,28 @@ class MyApp extends StatelessWidget {
 
   // This widget is the root of your application.
   static final navigatorKey = GlobalKey<NavigatorState>();
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
+      navigatorKey: navigatorKey, // Assigning the navigatorKey to the MaterialApp
+      initialRoute: '/', // Initial route is SignUpPage
       routes: {
-        '/spl': (context) => SplashScreen(
-          // Here, you can decide whether to show the LoginPage or HomePage based on user authentication
-          child: LoginPage(),
-        ),
-
         '/login': (context) => LoginPage(),
-        //'/signUp': (context) => SignUpPage(),
+        '/admin': (context) => AdminPage(),
         '/home': (context) => HomePage(),
+        // '/': (context) => QRView(
+        //   userId: "123",
+        //   date: "11.4.23",
+        //   slot: "slot1200",
+        // ),
+
         '/delete': (context) => DeleteAccount(),
-        // '/ticket':(context) => TicketBooking(),
         '/booking_page': (context) => BookingPage(user: ModalRoute.of(context)!.settings.arguments as User),
         '/success_booked': (context) => AppointmentBooked(),
-        '/': (context) => SignUpPage(), // Route to TicketBookingPage widget
-        '/admin' : (context) => AdminPage(),
+        '/': (context) => SignUpPage(), // Default route is SignUpPage
       },
     );
   }
