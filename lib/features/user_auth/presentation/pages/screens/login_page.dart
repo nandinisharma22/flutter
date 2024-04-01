95bb78c26ff81c8e8e289cfd3e28b64b1983da73
dwsds <146217248+dwsds@users.noreply.github.com>
2024-04-01 11:49:58 +0530

    Update login_page.dart

--- lib/features/user_auth/presentation/pages/screens/login_page.dart
+++ lib/features/user_auth/presentation/pages/screens/login_page.dart
@@ -1,152 +1,3 @@
-// import 'package:flutter/material.dart';
-// import 'package:firebase_auth/firebase_auth.dart';
-// import 'package:haxplore/features/user_auth/presentation/pages/screens/home_page.dart';
-// import 'package:haxplore/features/user_auth/presentation/pages/screens/signup_page.dart';
-// import 'package:haxplore/features/user_auth/presentation/widgets/form_container_widget.dart';
-//
-// import '../../../firebase_auth_implementation/firebase_auth_services.dart';
-//
-// class LoginPage extends StatefulWidget {
-//   const LoginPage({super.key});
-//
-//   @override
-//   State<LoginPage> createState() => _LoginPageState();
-// }
-//
-// class _LoginPageState extends State<LoginPage> {
-//   final FirebaseAuthServices _auth = FirebaseAuthServices();
-//   TextEditingController _emailController = TextEditingController();
-//   TextEditingController _passwordController = TextEditingController();
-//   String _errorMessage = '';
-//   @override
-//   void dispose() {
-//     _emailController.dispose();
-//     _passwordController.dispose();
-//
-//     super.dispose();
-//   }
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar:AppBar(
-//         backgroundColor: Colors.black,
-//         title: Text("Login", style: TextStyle(color: Colors.white),),
-//       ),
-//       backgroundColor: Colors.grey.shade600,
-//
-//       body:Padding(
-//         padding: const EdgeInsets.symmetric(horizontal: 15),
-//         child: Column(
-//           mainAxisAlignment: MainAxisAlignment.center,
-//           children: [
-//             Text("Login", style: TextStyle(fontSize: 50),),
-//             FormContainerWidget(
-//               controller: _emailController,
-//               hintText: "Email",
-//               isPasswordField: false,
-//             ),
-//             SizedBox(height: 10,),
-//             FormContainerWidget(
-//               controller: _passwordController,
-//               hintText: "Password",
-//               isPasswordField: true,
-//             ),
-//             SizedBox(height: 20,),
-//             Center(
-//               child: GestureDetector(
-//                 onTap: _signIn,
-//                 child: Container(
-//                   width: 150,
-//
-//                   height: 50,
-//                   decoration: BoxDecoration(
-//                     color: Colors.black,
-//                     borderRadius: BorderRadius.circular(10),
-//                   ),
-//                   child: Center(
-//                     child: Text(
-//                       "Login",
-//                       style: TextStyle(color: Colors.white),
-//                     ),
-//                   ),
-//                 ),
-//               ),
-//             ),
-//             SizedBox(height: 20,),
-//             SizedBox(height: 20),
-//
-//             // Display error message if it's not empty
-//             if (_errorMessage.isNotEmpty)
-//               Text(
-//                 _errorMessage,
-//                 style: TextStyle(color: Colors.black),
-//               ),
-//
-//             Row(mainAxisAlignment: MainAxisAlignment.center,
-//               children: [
-//                 Text("Dont have an account?"),
-//                 SizedBox(width: 5,),
-//                 GestureDetector(
-//                   onTap: (){
-//                     Navigator.pushAndRemoveUntil(context, MaterialPageRoute(builder: (context)=>SignUpPage()), (route) => false);
-//                   },
-//                   child: Text("Sign Up",style: TextStyle(color: Colors.red),),
-//                 )
-//               ],)
-//           ],
-//         ),
-//       ),
-//     );
-//   }
-//
-//   void _signIn() async {
-//     String email = _emailController.text;
-//     String password = _passwordController.text;
-//
-//     try {
-//       User? user = await _auth.signInWithEmailandPassword(email, password);
-//
-//       if (user == null) {
-//         setState(() {
-//           _errorMessage = "User ID does not exist.";
-//         });
-//       } else {
-//         print("User signed in successfully");
-//         Navigator.pushNamed(context, "/home");
-//       }
-//     } catch (error) {
-//       setState(() {
-//         // Display specific error messages to the user
-//         if (error is FirebaseAuthException) {
-//           if (error.code == "user-not-found") {
-//             _errorMessage = "User with this email does not exist.";
-//           } else {
-//             _errorMessage = _mapFirebaseErrorToMessage(error.code);
-//           }
-//         } else {
-//           _errorMessage = "An error occurred: $error";
-//         }
-//       });
-//       print("An error occurred: $error");
-//     }
-//   }
-//
-//
-//   // Map Firebase error codes to user-friendly messages
-//   String _mapFirebaseErrorToMessage(String errorCode) {
-//     switch (errorCode) {
-//       case "user-not-found":
-//         return "Email does not exist in the database. Please check your email.";
-//       case "wrong-password":
-//         return "Wrong password. Please try again.";
-//     // Add more cases for other Firebase error codes as needed
-//       default:
-//         return "An error occurred: $errorCode";
-//     }
-//   }
-//
-// }
-
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:haxplore/features/user_auth/presentation/pages/screens/home_page.dart';
@@ -154,9 +5,13 @@ import 'package:haxplore/features/user_auth/presentation/pages/screens/signup_pa
 import 'package:haxplore/features/user_auth/presentation/widgets/form_container_widget.dart';
 
 import '../../../firebase_auth_implementation/firebase_auth_services.dart';
+import 'package:flutter/material.dart';
+import 'package:haxplore/features/user_auth/firebase_auth_implementation/firebase_auth_services.dart';
+import 'package:haxplore/features/user_auth/presentation/pages/screens/signup_page.dart';
+import 'package:haxplore/features/user_auth/presentation/widgets/form_container_widget.dart';
 
 class LoginPage extends StatefulWidget {
-  const LoginPage({super.key});
+  const LoginPage({Key? key});
 
   @override
   State<LoginPage> createState() => _LoginPageState();
@@ -167,6 +22,7 @@ class _LoginPageState extends State<LoginPage> {
   TextEditingController _emailController = TextEditingController();
   TextEditingController _passwordController = TextEditingController();
   String _errorMessage = '';
+
   @override
   void dispose() {
     _emailController.dispose();
@@ -174,80 +30,71 @@ class _LoginPageState extends State<LoginPage> {
 
     super.dispose();
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar:AppBar(
-        backgroundColor: Colors.black,
-        title: Text("Login", style: TextStyle(color: Colors.white),),
+      appBar: AppBar(
+        title: Text("Login"),
       ),
-      backgroundColor: Colors.grey.shade600,
-
-      body:Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 15),
+      backgroundColor: Color(0x8BCBFFFF),
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text("Login", style: TextStyle(fontSize: 50),),
+            Text(
+              "Login",
+              style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 20),
             FormContainerWidget(
               controller: _emailController,
               hintText: "Email",
               isPasswordField: false,
             ),
-            SizedBox(height: 10,),
+            SizedBox(height: 10),
             FormContainerWidget(
               controller: _passwordController,
               hintText: "Password",
               isPasswordField: true,
             ),
-            SizedBox(height: 20,),
-            Center(
-              child: GestureDetector(
-                onTap: _signIn,
-                child: Container(
-                  width: 150,
-
-                  height: 50,
-                  decoration: BoxDecoration(
-                    color: Colors.black,
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                  child: Center(
-                    child: Text(
-                      "Login",
-                      style: TextStyle(color: Colors.white),
-                    ),
-                  ),
-                ),
-              ),
-            ),
-            SizedBox(height: 20,),
             SizedBox(height: 20),
-
-            // Display error message if it's not empty
+            ElevatedButton(
+              onPressed: _signIn,
+              child: Text("Login"),
+            ),
+            SizedBox(height: 10),
             if (_errorMessage.isNotEmpty)
               Text(
                 _errorMessage,
-                style: TextStyle(color: Colors.black),
+                style: TextStyle(color: Colors.red),
               ),
-
-            Row(mainAxisAlignment: MainAxisAlignment.center,
+            SizedBox(height: 10),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                Text("Dont have an account?"),
-                SizedBox(width: 5,),
+                Text("Don't have an account?"),
+                SizedBox(width: 5),
                 GestureDetector(
-                  onTap: (){
-                    Navigator.pushAndRemoveUntil(context, MaterialPageRoute(builder: (context)=>SignUpPage()), (route) => false);
+                  onTap: () {
+                    Navigator.pushReplacement(
+                      context,
+                      MaterialPageRoute(builder: (context) => SignUpPage()),
+                    );
                   },
-                  child: Text("Sign Up",style: TextStyle(color: Colors.red),),
-                )
-              ],)
+                  child: Text(
+                    "Sign Up",
+                    style: TextStyle(color: Colors.blue),
+                  ),
+                ),
+              ],
+            ),
           ],
         ),
       ),
     );
   }
-
   void _signIn() async {
     String email = _emailController.text;
     String password = _passwordController.text;
@@ -262,17 +109,15 @@ class _LoginPageState extends State<LoginPage> {
       } else {
         print("User signed in successfully");
 
-        if (user.email == "haxplore672@gmail.com") {
-          // Navigate to AdminPage
+        // Check the email to determine the redirection
+        if (email == 'haxplore672@gmail.com') {
           Navigator.pushReplacementNamed(context, "/admin");
         } else {
-          // Navigate to HomePage
           Navigator.pushReplacementNamed(context, "/home");
         }
       }
     } catch (error) {
       setState(() {
-        // Display specific error messages to the user
         if (error is FirebaseAuthException) {
           if (error.code == "user-not-found") {
             _errorMessage = "User with this email does not exist.";
@@ -288,7 +133,6 @@ class _LoginPageState extends State<LoginPage> {
   }
 
 
-
   // Map Firebase error codes to user-friendly messages
   String _mapFirebaseErrorToMessage(String errorCode) {
     switch (errorCode) {
@@ -296,10 +140,8 @@ class _LoginPageState extends State<LoginPage> {
         return "Email does not exist in the database. Please check your email.";
       case "wrong-password":
         return "Wrong password. Please try again.";
-    // Add more cases for other Firebase error codes as needed
       default:
         return "An error occurred: $errorCode";
     }
   }
-
 }
